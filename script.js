7325c6d2b40daea82dcf87389085aa4ea01619e7
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-02 18:43:33 +0100

    fine

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 "use strict";
 const playBoard = document.querySelector('.board');
-let difficult = 100;
+
+const playButton = document.querySelector('button');
+const playDifficult = document.querySelector('select');
+
+
+let difficult = "easy";
+let maxNumber = 100;
 startGame(difficult);
 
 function startGame(difficult){
     playBoard.innerHTML = "";
-    for (let i = 1; i <= difficult; i++) {
+
+    if(difficult === "easy"){
+        maxNumber = 100;
+    }else if(difficult === "medium"){
+        maxNumber = 81;
+    }else{
+        maxNumber = 49;
+    }
+    
+    for (let i = 1; i <= maxNumber; i++) {
         const playNumber = document.createElement("div");
         playNumber.innerHTML = i;
         playNumber.classList.add('board__number');  
         playNumber.addEventListener("click", function(){
             this.classList.add('selected');
         });
-        if(difficult === 100){
-            playNumber.style.width = calc(100 % 9);
-        }else if(difficult === 81){
-            playNumber.style.width = 100 % 7;
+
+        if(difficult === "easy"){
+            playNumber.style.width = "calc(100%/10)";
+            playNumber.style.height = "calc(100%/10)";
+        }else if(difficult === "medium"){
+            playNumber.style.width = "calc(100%/9)";
+            playNumber.style.height = "calc(100%/9)";
         }else{
-            playNumber.style.width = 100 % 7;
+            playNumber.style.width = "calc(100%/7)";
+            playNumber.style.height = "calc(100%/7)";
         }
+        console.log(playNumber);
         playBoard.append(playNumber);
     }
 }
+
+playButton.addEventListener("click", function(){
+    difficult = playDifficult.value;
+    startGame(difficult);
+    console.log(difficult);
+});
+
+
 
 // creo la tabella con tutti i numeri
 
