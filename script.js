0d71c54987213e98260dc38de317f8dc2c24ed6a
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-02 18:00:54 +0100

    creazione campo

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 "use strict";
+const playBoard = document.querySelector('.board');
+let difficult = 100;
+startGame(difficult);
 
+function startGame(difficult){
+    playBoard.innerHTML = "";
+    for (let i = 1; i <= difficult; i++) {
+        const playNumber = document.createElement("div");
+        playNumber.innerHTML = i;
+        playNumber.classList.add('board__number');  
+        playNumber.addEventListener("click", function(){
+            this.classList.add('selected');
+        });
+        if(difficult === 100){
+            playNumber.style.width = calc(100 % 9);
+        }else if(difficult === 81){
+            playNumber.style.width = 100 % 7;
+        }else{
+            playNumber.style.width = 100 % 7;
+        }
+        playBoard.append(playNumber);
+    }
+}
+
+// creo la tabella con tutti i numeri
+
+
+
+
+
+// console.log(this);
+// this.classList.add();
