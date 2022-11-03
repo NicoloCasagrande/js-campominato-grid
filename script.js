2c488a42c9a7eee6a5115600fe313df06872bdde
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-03 13:26:12 +0100

    aggiunte

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const playBoard = document.querySelector('.board');
 
 const playButton = document.querySelector('button');
 const playDifficult = document.querySelector('select');
+const points = document.querySelector('.punteggio');
 
 
 let difficult = "easy";
@@ -11,6 +12,9 @@ startGame(difficult);
 
 function startGame(difficult){
     playBoard.innerHTML = "";
+    let punteggio = 0;
+    points.innerHTML = punteggio;
+    let sconfitta = false;
 
     if(difficult === "easy"){
         maxNumber = 100;
@@ -19,13 +23,35 @@ function startGame(difficult){
     }else{
         maxNumber = 49;
     }
-    
+    const bombs = bombCreation(maxNumber);
+    let numSelected = [];
+
     for (let i = 1; i <= maxNumber; i++) {
         const playNumber = document.createElement("div");
         playNumber.innerHTML = i;
-        playNumber.classList.add('board__number');  
+        playNumber.classList.add('board__number');
+
         playNumber.addEventListener("click", function(){
-            this.classList.add('selected');
+                // this.removeEventListener("click", function(){});
+            if(bombs.includes(i)){
+                    this.classList.add('bomb');
+                    alert(`Hai perso con un punteggio di: ${punteggio}`);
+                    sconfitta = true;
+                    loseGame(bombs);
+            }else{
+                if(playNumber.classList.contains('selected')){
+                    playNumber.removeEventListener("click", function(){});
+                }else{
+                    punteggio += 1;
+                    points.innerHTML = punteggio;
+                    this.classList.add('selected');
+                    if(punteggio === maxNumber - 16){
+                        alert(`Hai vinto con un punteggio di: ${punteggio}`);
+                        playBoard.innerHTML = "";
+                    }
+                }
+            }
+            
         });
 
         if(difficult === "easy"){
@@ -38,16 +64,39 @@ function startGame(difficult){
             playNumber.style.width = "calc(100%/7)";
             playNumber.style.height = "calc(100%/7)";
         }
-        console.log(playNumber);
         playBoard.append(playNumber);
     }
+    console.log(bombs);
 }
 
 playButton.addEventListener("click", function(){
     difficult = playDifficult.value;
     startGame(difficult);
-    console.log(difficult);
 });
+
+function bombCreation(maxNumber){
+    let bombPosition = [];
+    while(bombPosition.length < 16){
+        const randomNumber = msMathRandom(maxNumber);
+        if(!bombPosition.includes(randomNumber)){
+            bombPosition.push(randomNumber);
+        }
+    }
+    return bombPosition;
+}
+
+function msMathRandom(nMax){
+    const result = Math.floor(Math.random() * nMax) + 1;
+    return result;
+}
+
+function loseGame(bombsArray){
+    for (let i = 0; i < bombsArray.length; i++) {
+        console.log(bombsArray.value);
+        const bombsDiv = querySelector(`.board__number:nth-child(${bombsArray.value})`);
+        bombsDiv.classList.add('bomb');
+    }
+}
 
 
 
